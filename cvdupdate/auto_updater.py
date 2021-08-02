cf5f3ecd835bcb5fe29dd712366a3c514582e839
Philippe Ballandras <pballandras@coveo.com>
2021-08-02 17:04:02 -0400

    doc and behaviour changes

--- cvdupdate/auto_updater.py
+++ cvdupdate/auto_updater.py
@@ -2,21 +2,23 @@ from threading import Event, Thread
 
 from cvdupdate.cvdupdate import CVDUpdate
 
-# one time per day
-WAIT_TIME_SECONDS = 24 * 60 * 60
+def start(interval: int) -> None:
+    '''Spawns a thread to update the AV db after "interval" seconds
+    :param interval: the interval in seconds between 2 updates of the db
+    '''
+    if interval > 0:
+        Thread(target=_update, daemon=True, args=[interval]).start()
 
 
-def start() -> None:
-    """Spawns a thread to update the AV db periodically"""
-    Thread(target=_update, daemon=True).start()
+def _update(interval: int) -> None:
+    '''Don't call this directly
 
-
-def _update() -> None:
-    """Don't call this directly
-    Updates the AV db every day when it was started"""
+    Updates the AV db after every "interval" seconds when it was started
+    :param interval: the interval in seconds between 2 updates of the db
+    '''
     ticker = Event()
     m = CVDUpdate()
-    while not ticker.wait(WAIT_TIME_SECONDS):
+    while not ticker.wait(interval):
         errors = m.db_update(debug_mode=True)
         if errors > 0:
-            m.logger.error(f"Failed to fetch updates from ClamAV databases")
+            m.logger.error("Failed to fetch updates from ClamAV databases")
